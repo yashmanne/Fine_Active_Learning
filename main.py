f9efddb40dcfcb2da786dc5152e6d627f64b5788
ajnetzley <anetzley@uw.edu>
2024-03-05 22:28:09 -0800

    Final main push

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import wandb_utils as wbu
 import importlib
 importlib.reload(wbu)
 datasets = ['DTD']
-num_samples = [10]
+num_samples = [10, 20]
 al_methods = ['K-Medoids', 'LSS']
-random_seeds = [0]
+random_seeds = [0, 1, 2]
 hyperparameter_kwargs = {'max_runs':2, 'method':'grid', 'num_epochs':50}
 wbu.run_all_combinations(datasets, num_samples, al_methods, random_seeds, hyperparameter_kwargs)
