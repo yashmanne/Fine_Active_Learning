3107c0f2d1192558d06aba88e61c435b9d3266de
Yash Manne <manneyas@msu.edu>
2024-03-06 00:22:39 -0800

    script for google Cloud

---
+++ run_ProbAL.py
@@ -0,0 +1,10 @@
+import wandb
+import wandb_utils as wbu
+
+wandb.login(key='67f26507b9d2f6c1bec3b0101751fa761b3a30d2')
+datasets = ['DTD']
+num_samples = [10, 20]
+al_methods = ['Entropy', 'LeastConfidence', 'LeastMargin']
+random_seeds = [0, 1, 2]
+hyperparameter_kwargs = {'max_runs':2, 'method':'grid', 'num_epochs':50}
+wbu.run_all_combinations(datasets, num_samples, al_methods, random_seeds, hyperparameter_kwargs)
