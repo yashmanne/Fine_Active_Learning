615224cc2736fc3a3e369724028b69b60d4b2b9c
ajnetzley <anetzley@uw.edu>
2024-03-05 21:56:33 -0800

    Main changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import importlib
 importlib.reload(wbu)
 datasets = ['DTD']
 num_samples = [10]
-al_methods = ['K-Medoids']
-random_seeds = [1]
-hyperparameter_kwargs = {'max_runs':2, 'method':'grid', 'num_epochs':5}
+al_methods = ['K-Medoids', 'LSS']
+random_seeds = [0]
+hyperparameter_kwargs = {'max_runs':2, 'method':'grid', 'num_epochs':50}
 wbu.run_all_combinations(datasets, num_samples, al_methods, random_seeds, hyperparameter_kwargs)
